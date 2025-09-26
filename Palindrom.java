63e7e302433d1f4bd7c8454bbf9fda6cb78a73a8
Yash verma <yashverma9084+github@gmail.com>
2025-09-27 01:36:16 +0530

    commit

---
+++ Palindrom.java
@@ -0,0 +1,36 @@
+
+public class Palindrom {
+
+    static boolean isPalindrom(int i) {
+        int temp = i;
+        int num = 0;
+        while (i != 0) {
+            int k = i % 10;
+            num = num * 10 + k;
+            i /= 10;
+        }
+        if (temp == num) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {111, 222, 333, 444, 577};
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            boolean flag = isPalindrom(i);
+            if (flag) {
+                count++;
+            }
+        }
+        if (count == arr.length) {
+            System.out.println("Number is palindrom");
+        } else {
+            System.out.println("Not palindrom");
+        }
+
+    }
+}
